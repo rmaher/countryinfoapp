b2a411b1bae647ea427a70e22847b363adbf1362
Rachel Maher <rachelelizabethmaher@gmail.com>
2015-06-27 13:51:30 -0600

    working images

--- countriesapp.js
+++ countriesapp.js
@@ -13,42 +13,75 @@ angular.module('CountriesApp', ['ngRoute'])
              }
             }
     	})
-        .when('/country/:countryName', {
+        .when('/country/:countryCode', {
             templateUrl: 'countrydetail.html',
             controller: 'CountryCtrl',
             resolve : {
                 country: function($route, $location, $http, $q) {
-                    var countryName = $route.current.params.countryName;
+                    var countryCode = $route.current.params.countryCode;
 
-                    var countryUrl = "http://api.geonames.org/countryInfoJSON?username=rachelelizabethmaher&country=" + $countryName;
+                    var countryUrl = "http://api.geonames.org/countryInfoJSON?username=rachelelizabethmaher&country=" + countryCode;
                     var deferred = $q.defer();
 
                     $http.get(countryUrl).then( function(response) {
-                            deferred = $q.defer();
+                            deferred.resolve(response.data.geonames);
                         }, function(error) {
-                            deferred.reject('error!')
-                        })
+                            deferred.reject('error!');
+                        });
  
                         return deferred.promise;
-                    
-                    },
                 }
-        })
+                //capitals: function($http, $q) {
+                    //var capitalUrl = "http://api.geonames.org/searchJSON?username=rachelelizabethmaher&country=" + countryCode + "&q=" + capital;
+                    //var deferred = $q.defer();
+
+                    //$http.get(capitalUrl).then( function(response) {
+                            //deferred.resolve(response.data.geonames);
+                        //}, function(error) {
+                            //deferred.reject('error!');
+                        //});
+ 
+                        //return deferred.promise;
+
+                //}
+
+                //neighbours: function($http, $q) {
+                    //var neighborUrl = "http://api.geonames.org/neighbours?geonameId=" + geonameId + "&username=rachelelizabethmaher";
+                    //var deferred = $q.defer();
+
+                    //$http.get(neighborUrl).then( function(response) {
+                            //deferred.resolve(response.data.geonames);
+                        //}, function(error) {
+                            //deferred.reject('error!');
+                        //});
+ 
+                        //return deferred.promise;
+                
+                //}
+
+
+        }})
     	.when('/error', {
     		template: '<p>Error - Page Not Found</p>'
     	})
-    	.otherwise({redirectTo: '/'})
+    	.otherwise({redirectTo: '/'});
     }) 
 
-    .controller('MainCtrl', function($scope, $routeParams) {
+    .controller('MainCtrl', function($scope) {
 
     })
 
-    .controller('ListCtrl', function($scope, $routeParams) {
-  
+    .controller('ListCtrl', function($scope, countries) {
+      $scope.countries = countries.data.geonames;
     		
     })
 
-    .controller('CountryCtrl', function($scope, $http) {
+    .controller('CountryCtrl', function($scope, country) {
+      
+      $scope.country = country[0];   
 
+
+      //$scope.neighbours = neighbours[0]
+      //console.log(neighbours);
+    });
 
--- countriesgraveyard.js
+++ countriesgraveyard.js
@@ -51,3 +51,60 @@
                 console.log("ERROR ERROR")
             });
     })
+
+
+    ////////
+
+    angular.module('CountriesApp', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl : 'home.html',
+        })
+        .when('/countrylist', {
+            templateUrl: 'countrylist.html',
+            controller: 'ListCtrl',
+          resolve: {
+             countries: function($http) {
+               var countryUrl = "http://api.geonames.org/countryInfoJSON?username=rachelelizabethmaher";
+               return $http.get(countryUrl);
+             }
+          }
+        })
+        .when('/country/:countryName', {
+            templateUrl: 'countrydetail.html',
+            controller: 'CountryCtrl',
+            resolve : {
+                country: function($route, $location, $http, $q) {
+                    var countryName = $route.current.params.countryName;
+
+                    var countryUrl = "http://api.geonames.org/countryInfoJSON?username=rachelelizabethmaher&country=" + countryName;
+                    var deferred = $q.defer();
+
+                    $http.get(countryUrl).then( function(response) {
+                            deferred.resolve(response);
+                        }, function(error) {
+                            deferred.reject('error!');
+                        });
+
+                    return deferred.promise;
+
+                }
+        }})
+        .when('/error', {
+            template: '<p>Error - Page Not Found</p>'
+        })
+        .otherwise({redirectTo: '/'});
+    })
+
+    .controller('MainCtrl', function($scope, $routeParams) {
+
+    })
+
+    .controller('ListCtrl', function($scope, $routeParams, countries) {
+      $scope.countries = countries.data.geonames;
+    })
+
+    .controller('CountryCtrl', function($scope, $http, country) {
+      console.log(country);
+      $scope.country = country;
+    });
